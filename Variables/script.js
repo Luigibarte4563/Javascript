51fe1f02f8811c486f73b03c2c5abf488b147eb3
Luigibarte4563 <luigibarte1@gmail.com>
2026-06-03 20:59:09 +0800

    html

--- Variables/script.js
+++ Variables/script.js
@@ -1,5 +1,14 @@
+// let
 let name = "Luigi";
-let age = 20
-let gender = 'M'
+let age = 20;
+let gender = 'M';
+let isStudent = true;
 
-console.log(`my name is ${name}, age is ${age}, gender ${gender}`);
+console.log(`name: ${name}`);
+console.log(`age: ${age}`);
+console.log(`gender: ${gender}`);
+console.log(`isStudent? ${isStudent}`);
+
+// const
+const PI = 3.14159;
+console.log(PI);
