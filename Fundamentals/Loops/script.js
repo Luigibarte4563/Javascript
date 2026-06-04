5b05193665fff3aa70b6683911424d8990258cea
Luigibarte4563 <luigibarte1@gmail.com>
2026-06-04 20:59:13 +0800

    loops

---
+++ Fundamentals/Loops/script.js
@@ -0,0 +1,18 @@
+// for loop
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+for (let i = 2; i <= 10; i += 2) {
+    console.log(i);
+}
+
+// while loop
+
+let i = 5;
+
+while (i >= 0) {
+    console.log(i);
+    i--;
+}
