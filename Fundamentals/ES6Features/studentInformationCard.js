e74727e1729fa4674d4881de4144caf7b5a067c2
Luigibarte4563 <luigibarte1@gmail.com>
2026-07-02 20:08:18 +0800

    studentInformationCard

---
+++ Fundamentals/ES6Features/studentInformationCard.js
@@ -0,0 +1,17 @@
+const student = {
+    name: "Luigi",
+    age: 20,
+    course: "BSIT"
+};
+
+const {name, age, course} = student;
+
+console.log(`
+Student Information
+
+Name: ${name}
+Age: ${age}
+Course: ${course}
+
+`);
+
