5578bc89a13440f05b04e9fec13b760cb5faf0e3
Luigibarte4563 <luigibarte1@gmail.com>
2026-07-18 22:04:41 +0800

    basic syntax EDP

---
+++ EventDrivenProgramming/BasicSyntax/script.js
@@ -0,0 +1,11 @@
+// element.addEventListener("event", function);
+
+// button.addEventListener("click", function() {
+//     console.log("Hello");
+// });
+
+let button = document.getElementById("button");
+
+button.addEventListener("click", function() {
+    alert("Hello");
+});
