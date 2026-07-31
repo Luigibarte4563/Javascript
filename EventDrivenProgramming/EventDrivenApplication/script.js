399438f98819ab1f793ce996eaf057549372cbef
Luigi Barts <Luigibarte4563>
2026-07-31 22:07:44 +0800

    edp applicatioon

---
+++ EventDrivenProgramming/EventDrivenApplication/script.js
@@ -0,0 +1,7 @@
+const input = document.getElementById("studentName");
+
+const button = document.getElementById("addStudent");
+
+const list = document.getElementById("studentList");
+
+const total = document.getElementById("total");
