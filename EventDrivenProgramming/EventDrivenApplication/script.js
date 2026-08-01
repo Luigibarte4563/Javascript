6e5c2570ff441b556f42d9a171daf73c93aba5ab
Luigibarte4563 <luigibarte1@gmail.com>
2026-08-01 21:11:19 +0800

    search event delegation

--- EventDrivenProgramming/EventDrivenApplication/script.js
+++ EventDrivenProgramming/EventDrivenApplication/script.js
@@ -1,7 +1,36 @@
 const input = document.getElementById("studentName");
-
 const button = document.getElementById("addStudent");
-
 const list = document.getElementById("studentList");
-
 const total = document.getElementById("total");
+
+button.addEventListener('click', addStudent);
+
+function addStudent() {
+    const name = input.value.trim();
+    if (name === "") return;
+    const li = document.createElement("li");
+    li.testContent = name;
+    list.appendChild(li);
+
+    updateCounter();
+}
+
+function updateCounter() {
+    total.textContent = 
+        list.children.length + " Students";
+}
+
+// Search
+search.addEventListener('input', searchStudent);
+
+function searchStudent() {
+    console.log(search.value);
+}
+
+// List
+
+list.addEventListener('click', function(event){
+    if(event.target.tagName === "LI") {
+        event.target.remove();
+    }
+});
