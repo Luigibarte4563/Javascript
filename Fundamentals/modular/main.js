59dc430fa64a8a747629db5f80acf8c5597e871e
Luigi Barts <Luigibarte4563>
2026-08-22 20:24:03 +0800

    print

--- Fundamentals/modular/main.js
+++ Fundamentals/modular/main.js
@@ -1,4 +1,7 @@
 import {add, subtract} from './math.js';
+import { students, getAverageGrade } from './student.js';
 
 console.log(add(5,10));
 console.log(subtract(5,10));
+
+console.log(students[0].name);
