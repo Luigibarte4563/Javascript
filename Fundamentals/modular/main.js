bef044ccd8ba13a7a175ef4b0386b42409973b6a
Luigi Barts <luigibarte1@gmail.com>
2026-08-22 21:22:52 +0800

    search name and grade

--- Fundamentals/modular/main.js
+++ Fundamentals/modular/main.js
@@ -6,3 +6,21 @@ console.log(subtract(5,10));
 
 console.log(students[0].name);
 console.log(students[1].name);
+
+let label = document.querySelector("#label");
+let search = document.querySelector("#search");
+
+function searchNameGrade() {
+    const number = Number(search.value);
+
+    if (number >= 1 && number <= students.length) {
+        const student = students[number - 1];
+
+        label.textContent = `Name: ${student.name} - Grade: ${student.grade}`;
+    } else {
+        label.textContent = "Student not found";;
+    }
+}
+
+search.addEventListener('input', searchNameGrade);
+
