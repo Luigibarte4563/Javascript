6d3ff45193d0ff4e737aeee136b151c396b5f357
Luigi Barts <luigibarte1@gmail.com>
2026-08-25 22:17:17 +0800

    Logical Operators

---
+++ Fundamentals/LogicalOperators/script.js
@@ -0,0 +1,13 @@
+let grade = 85;
+let attendance = 90;
+
+console.log(grade >= 75 && attendance >= 80);
+
+let hasID = true;
+let hasPassport = false;
+
+console.log(hasID || hasPassport);
+
+let isLoggedIn = false;
+
+console.log(!isLoggedIn);
