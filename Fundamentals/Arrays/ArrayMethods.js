b46a4759bf75035b6960a017ca595b4758511c9d
Luigibarte4563 <luigibarte1@gmail.com>
2026-09-04 22:32:00 +0800

    filter()

--- Fundamentals/Arrays/ArrayMethods.js
+++ Fundamentals/Arrays/ArrayMethods.js
@@ -15,3 +15,9 @@ fruits.forEach(function(fruit) {
 fruits.forEach(fruits => {
     console.log(fruit);
 });
+
+const numbers = [1, 2, 3, 4, 5];
+
+const result = numbers.filter(number >= number > 2);
+
+comsole.log(result);
