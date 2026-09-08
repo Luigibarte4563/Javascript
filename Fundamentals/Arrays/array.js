ba3d5bc8abc412b8d0c62caa7b14cf360fefa8d8
Luigibarte4563 <luigibarte1@gmail.com>
2026-09-08 21:26:01 +0800

    map

--- Fundamentals/Arrays/array.js
+++ Fundamentals/Arrays/array.js
@@ -8,3 +8,9 @@ let students = [
 
 console.log(students);
 console.log(students[0]); // Luigi
+
+// map
+let numbers = [1, 2, 3];
+const doubleNumber = numbers.map(number => {
+    return number * 2;
+})
